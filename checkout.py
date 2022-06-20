092d725a26316a36ea632d80355bd361ed3a619f
Oluwasayo Ogundipe <eyisayo444@gmail.com>
2022-06-20 23:07:16 +0100

    Item A and B price calcs implemented

--- checkout.py
+++ checkout.py
@@ -1,6 +1,31 @@
-def checkoutKata(var):
-    print(var)
-    print("second change")
+'''Checkout Kata function that implements the proposed supermarket checkout system
+   The input to the function is a string of all the items e.g. CCC, ACC
+
+'''
+
+from collections import Counter
 
 
-checkoutKata("hello")   
+def checkoutKata(items):
+    items = items.upper()
+
+    itemDict = Counter(items)
+
+    aCount = itemDict["A"]
+    bCount = itemDict["B"]
+    cCount = itemDict["C"]
+
+    aPrice = aCount * 0.50
+    bPrice = (bCount//2 * 1.25) + 0.75
+
+    #print(aPrice, bPrice)
+
+
+    
+
+       
+
+
+
+
+checkoutKata("AAAABBBBBBBCCCCCCCC")   
