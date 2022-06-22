fe37805509a14af839290a52344c8a972d175d5a
Oluwasayo Ogundipe <eyisayo444@gmail.com>
2022-06-22 22:03:07 +0100

    Implemented the 3rd, 4th and 5th test cases

--- pyunittest.py
+++ pyunittest.py
@@ -1,3 +1,4 @@
+from tabnanny import check
 import checkout    # The code to test
 import unittest   # The test framework
 
@@ -6,7 +7,16 @@ class TestcheckoutValue(unittest.TestCase):
         self.assertEqual(checkout.checkoutKata("CCC"), 0.75)
 
     def testCaseTwo(self):
-        self.assertEqual(checkout.checkoutKata("AAC"), 1.25)    
+        self.assertEqual(checkout.checkoutKata("AAC"), 1.25)  
+
+    def testCaseThree(self):
+        self.assertEqual(checkout.checkoutKata("BBB"), 2.00)   
+
+    def testCaseFour(self):
+        self.assertEqual(checkout.checkoutKata("CCCC"), 0.75)   
+
+    def testCaseFive(self):
+        self.assertEqual(checkout.checkoutKata("BBCCCC"), 2.00)        
 
 if __name__ == '__main__':
     unittest.main()
